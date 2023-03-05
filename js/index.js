e559439f6b74e9babb874c8557fce42bb379ffcc
Bealeja <117786111+Bealeja@users.noreply.github.com>
2023-03-05 18:55:51 +1100

    Changes to forms

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-function myFunction() {
+function tacFunction() {
   var popup = document.getElementById("tacid");
-  popup.classList.toggle("show");
+  popup.classList.toggle("tacshow");
+}
+
+function formFunction() {
+  var popup = document.getElementById("formid");
+  popup.classList.toggle("formshow");
 }
